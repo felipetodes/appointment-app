0a8869d7c6b7c8c79b2855ddd73d26b5c7e14dae
Felipe Todeschini <todeschini1992@gmail.com>
2025-02-06 20:30:13 -0300

    atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppointmentListComponent } from "./appointment-list/appointment-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:  [AppointmentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  imports: [],
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css'
+})
+export class AppointmentListComponent {
+
+
+  appointment: string  = "Pegue o cachorro para passear";
+}
